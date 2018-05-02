934cd13f63b14915c8eeda3d4849ff18c6b04e7c
Angel <a.peraza@outlook.com>
2018-05-02 01:11:58 -0600

    inicio actualizado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,133 +5,96 @@
 @section('content')
 
 <div class="wrapper wrapper-content animated fadeInRight">
+
   <div class="row">
-    <div class="col-lg-4">
-      <a href="{{ url('reporte/date') }}">
-        <div class="widget yellow-bg p-lg text-center">
-          <div class="m-b-md">
-            <i class="fa fa-file fa-4x"></i>
-            <h3 class="font-bold no-margins">
-              Reportes por fecha
-            </h3>
-            <small>Reportes</small>
+
+    <div class="col-md-8">
+
+      <div class="row">
+          <div class="col-md-6">
+              <div class="ibox float-e-margins">
+                  <div class="ibox-title">
+                      <span class="label label-info pull-right">Mensual</span>
+                      <h5>Biopsias</h5>
+                  </div>
+                  <div class="ibox-content">
+                      <h1 class="no-margins">{{ $biopsias }}</h1>
+                      <small>Biopsias del mes</small>
+                  </div>
+              </div>
           </div>
-        </div>
-      </a>
+          <div class="col-md-6">
+              <div class="ibox float-e-margins">
+                  <div class="ibox-title">
+                      <span class="label label-primary pull-right">Mensual</span>
+                      <h5>Citologías</h5>
+                  </div>
+                  <div class="ibox-content">
+                      <h1 class="no-margins">{{ $citologias }}</h1>
+                      <small>Citologías del mes</small>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
 
-    <div class="col-lg-4">
-      <a href="{{ url('biopsia/create') }}">
-        <div class="widget navy-bg p-lg text-center">
-            <div class="m-b-md">
-                <i class="fa fa-medkit fa-4x"></i>
-                <h3 class="font-bold no-margins">
-                    Crear Biopsia
-                </h3>
-                <small>Biopsia</small>
+    <div class="col-md-4">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Accesos directos
             </div>
-        </div>
-      </a>
-    </div>
-
-    <div class="col-lg-4">
-      <a href="{{ url('citologia/create') }}">
-        <div class="widget lazur-bg p-lg text-center">
-            <div class="m-b-md">
-                <i class="fa fa-heartbeat fa-4x"></i>
-                <h3 class="font-bold no-margins">
-                    Crear Citología
-                </h3>
-                <small>Citología</small>
-            </div>
-        </div>
-      </a>
-    </div>
-
-
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-success pull-right">Día</span>
-                <h5>Exámenes</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins">{{ $dia }}</h1>
-                <small>Exámenes del día</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-info pull-right">Mensual</span>
-                <h5>Biopsias</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins">{{ $biopsias }}</h1>
-                <small>Biopsias del mes</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-primary pull-right">Mensual</span>
-                <h5>Citologías</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins">{{ $citologias }}</h1>
-                <small>Citologías del mes</small>
-            </div>
+            <div class="list-group">
+              <a href="{{ url('biopsia/create') }}" class="list-group-item">Crear Biopsia</a>
+              <a href="{{ url('citologia/create') }}" class="list-group-item">Crear Citología</a>
+            </div>           
         </div>
     </div>
   </div>
 
   <div class="row">
-
     <div class="col-md-12">
-        <div class="ibox float-e-margins">
-          <div class="ibox-title">
-            <h5>Exámenes del mes</h5>
-          </div>
-          <div class="ibox-content">
-            <div class="table-responsive">
-              <table id="tblmeses" class="table table-bordered table-striped">
-                <thead>
+      <div class="ibox float-e-margins">
+        <div class="ibox-title">
+          <h5>Exámenes del mes</h5>
+        </div>
+        <div class="ibox-content">
+          <div class="table-responsive">
+            <table id="tblmeses" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Tipo</th>
+                  <th>Informe</th>
+                  <th>Estado</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($meses as $mes)
                   <tr>
-                    <th>Tipo</th>
-                    <th>Informe</th>
-                    <th>Estado</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach ($meses as $mes)
-                    <tr>
-                      <td>
-                        @if ($mes->tipo == "B")
-                          Biopsia
-                        @else
-                          Citología
+                    <td>
+                      @if ($mes->tipo == "B")
+                        Biopsia
+                      @else
+                        Citología
+                      @endif
+                    </td>
+                    <td>{{ $mes->informe }}</td>
+                    <td>
+                      @foreach ($pagos as $pago)
+                        @if ($pago['value'] == $mes->estado_pago)
+                          {{  $pago['text'] }}
                         @endif
-                      </td>
-                      <td>{{ $mes->informe }}</td>
-                      <td>
-                        @foreach ($pagos as $pago)
-                          @if ($pago['value'] == $mes->estado_pago)
-                            {{  $pago['text'] }}
-                          @endif
-                        @endforeach
-                      </td>
-                    </tr>
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
+                      @endforeach
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
           </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  </div>
 </div>
 @endsection
 
--- storage/framework/views/494004430308c73513e3e87175be8fc586496854.php
+++ storage/framework/views/494004430308c73513e3e87175be8fc586496854.php
@@ -3,134 +3,97 @@
 <?php $__env->startSection('content'); ?>
 
 <div class="wrapper wrapper-content animated fadeInRight">
+
   <div class="row">
-    <div class="col-lg-4">
-      <a href="<?php echo e(url('reporte/date')); ?>">
-        <div class="widget yellow-bg p-lg text-center">
-          <div class="m-b-md">
-            <i class="fa fa-file fa-4x"></i>
-            <h3 class="font-bold no-margins">
-              Reportes por fecha
-            </h3>
-            <small>Reportes</small>
+
+    <div class="col-md-8">
+
+      <div class="row">
+          <div class="col-md-6">
+              <div class="ibox float-e-margins">
+                  <div class="ibox-title">
+                      <span class="label label-info pull-right">Mensual</span>
+                      <h5>Biopsias</h5>
+                  </div>
+                  <div class="ibox-content">
+                      <h1 class="no-margins"><?php echo e($biopsias); ?></h1>
+                      <small>Biopsias del mes</small>
+                  </div>
+              </div>
           </div>
-        </div>
-      </a>
+          <div class="col-md-6">
+              <div class="ibox float-e-margins">
+                  <div class="ibox-title">
+                      <span class="label label-primary pull-right">Mensual</span>
+                      <h5>Citologías</h5>
+                  </div>
+                  <div class="ibox-content">
+                      <h1 class="no-margins"><?php echo e($citologias); ?></h1>
+                      <small>Citologías del mes</small>
+                  </div>
+              </div>
+          </div>
+      </div>
     </div>
 
-    <div class="col-lg-4">
-      <a href="<?php echo e(url('biopsia/create')); ?>">
-        <div class="widget navy-bg p-lg text-center">
-            <div class="m-b-md">
-                <i class="fa fa-medkit fa-4x"></i>
-                <h3 class="font-bold no-margins">
-                    Crear Biopsia
-                </h3>
-                <small>Biopsia</small>
+    <div class="col-md-4">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                Accesos directos
             </div>
-        </div>
-      </a>
-    </div>
-
-    <div class="col-lg-4">
-      <a href="<?php echo e(url('citologia/create')); ?>">
-        <div class="widget lazur-bg p-lg text-center">
-            <div class="m-b-md">
-                <i class="fa fa-heartbeat fa-4x"></i>
-                <h3 class="font-bold no-margins">
-                    Crear Citología
-                </h3>
-                <small>Citología</small>
-            </div>
-        </div>
-      </a>
-    </div>
-
-
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-success pull-right">Día</span>
-                <h5>Exámenes</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins"><?php echo e($dia); ?></h1>
-                <small>Exámenes del día</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-info pull-right">Mensual</span>
-                <h5>Biopsias</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins"><?php echo e($biopsias); ?></h1>
-                <small>Biopsias del mes</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="ibox float-e-margins">
-            <div class="ibox-title">
-                <span class="label label-primary pull-right">Mensual</span>
-                <h5>Citologías</h5>
-            </div>
-            <div class="ibox-content">
-                <h1 class="no-margins"><?php echo e($citologias); ?></h1>
-                <small>Citologías del mes</small>
-            </div>
+            <div class="list-group">
+              <a href="<?php echo e(url('biopsia/create')); ?>" class="list-group-item">Crear Biopsia</a>
+              <a href="<?php echo e(url('citologia/create')); ?>" class="list-group-item">Crear Citología</a>
+            </div>           
         </div>
     </div>
   </div>
 
   <div class="row">
-
     <div class="col-md-12">
-        <div class="ibox float-e-margins">
-          <div class="ibox-title">
-            <h5>Exámenes del mes</h5>
-          </div>
-          <div class="ibox-content">
-            <div class="table-responsive">
-              <table id="tblmeses" class="table table-bordered table-striped">
-                <thead>
+      <div class="ibox float-e-margins">
+        <div class="ibox-title">
+          <h5>Exámenes del mes</h5>
+        </div>
+        <div class="ibox-content">
+          <div class="table-responsive">
+            <table id="tblmeses" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Tipo</th>
+                  <th>Informe</th>
+                  <th>Estado</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php $__currentLoopData = $meses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mes): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                   <tr>
-                    <th>Tipo</th>
-                    <th>Informe</th>
-                    <th>Estado</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php $__currentLoopData = $meses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mes): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                    <tr>
-                      <td>
-                        <?php if($mes->tipo == "B"): ?>
-                          Biopsia
-                        <?php else: ?>
-                          Citología
-                        <?php endif; ?>
-                      </td>
-                      <td><?php echo e($mes->informe); ?></td>
-                      <td>
-                        <?php $__currentLoopData = $pagos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pago): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                          <?php if($pago['value'] == $mes->estado_pago): ?>
-                            <?php echo e($pago['text']); ?>
+                    <td>
+                      <?php if($mes->tipo == "B"): ?>
+                        Biopsia
+                      <?php else: ?>
+                        Citología
+                      <?php endif; ?>
+                    </td>
+                    <td><?php echo e($mes->informe); ?></td>
+                    <td>
+                      <?php $__currentLoopData = $pagos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pago): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <?php if($pago['value'] == $mes->estado_pago): ?>
+                          <?php echo e($pago['text']); ?>
 
-                          <?php endif; ?>
-                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                      </td>
-                    </tr>
-                  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                </tbody>
-              </table>
-            </div>
+                        <?php endif; ?>
+                      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </td>
+                  </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+              </tbody>
+            </table>
           </div>
+        </div>
+      </div>
     </div>
   </div>
 
-  </div>
 </div>
 <?php $__env->stopSection(); ?>
 
